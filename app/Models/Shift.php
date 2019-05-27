6b9eede8e3ccae83b16f0441df9dbb3c88e24040
Taiye <taiyeoguns@yahoo.com>
2019-05-27 22:52:16 +0100

    Add helper methods to shift model

--- app/Models/Shift.php
+++ app/Models/Shift.php
@@ -76,12 +76,19 @@ class Shift extends Model
 
     /**
      * Tasks for this shift
+     *
+     * @return void
      */
     public function tasks()
     {
         return $this->hasMany(Task::class);
     }
 
+    /**
+     * Get tasks for currently logged in user
+     *
+     * @return void
+     */
     public function userTasks()
     {
         if (auth()->user()->isMember()) {
@@ -89,5 +96,70 @@ class Shift extends Model
         } else {
             return $this->tasks;
         }
+    }
+
+    /**
+     * Check if shift is for current day
+     */
+    public function isToday()
+    {
+        return $this->shift_date->eq(Carbon::today());
+    }
+
+    /**
+     * Gets groups data for Vis Timeline
+     *
+     * @return void
+     */
+    public function getGroupsData()
+    {
+        return $this->members->map(function ($member) {
+            $data = [];
+            $data['id'] = $member->id;
+            $data['content'] = $member->user->name;
+
+            return $data;
+        });
+    }
+
+    /**
+     * Gets data for Vis Timeline
+     *
+     * @return void
+     */
+    public function getTimelineData()
+    {
+        return $this->userTasks()->map(function ($task) {
+            $data = [];
+            $data['id'] = $task->id;
+            $data['content'] = $task->title;
+            $data['group'] = $task->owner->id;
+            $data['start'] = $task->start->toDateTimeString();
+            if ($task->end != null) {
+                $data['end'] = $task->end->toDateTimeString();
+            }
+
+            return $data;
+        });
+    }
+
+    /**
+     * Gets task count for each member in this shift
+     *
+     * @return void
+     */
+    public function getMemberTasks()
+    {
+        return $this->members->map(function ($member) {
+            $data = [];
+            $data['member'] = $member->user->name;
+            $data['count'] = $this->userTasks()->filter(function ($task) use ($member) {
+                return $task->owner->id == $member->id;
+            })->count();
+
+            return $data;
+        })->filter(function ($task) {
+            return $task['count'] > 0;
+        });
     }
 }
